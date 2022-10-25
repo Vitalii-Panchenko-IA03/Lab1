4f90f4e569f8958efa20ade13409307a38226d5e
rluCvVWc <panv1t2l1k@gmail.com>
2022-10-25 14:50:35 +1100

    .eslintrc.js formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: ['standard', 'plugin:sonarjs/recommended', 'prettier'],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  plugins: ['sonarjs'],
-  rules: {
-  }
-}
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['standard', 'plugin:sonarjs/recommended', 'prettier'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    plugins: ['sonarjs'],
+    rules: {},
+};
