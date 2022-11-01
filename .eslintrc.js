945dde2cb123a620b074aecde9d26ffa0c751004
rluCvVWc <panv1t2l1k@gmail.com>
2022-11-01 18:04:49 +1100

    change prettier tabwidth, prettier rewrite

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-    env: {
-        node: true,
-        es2021: true,
-    },
-    extends: [
-        'standard',
-        'eslint:recommended',
-        'plugin:sonarjs/recommended',
-        'plugin:import/recommended',
-        'prettier',
-    ],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: 'latest',
-    },
-    plugins: ['sonarjs'],
-    rules: {},
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'standard',
+    'eslint:recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['sonarjs'],
+  rules: {},
 };
--- api/message.js
+++ api/message.js
@@ -1,3 +1,3 @@
 module.exports = (req, res) => {
-    res.status(200).send(`This is Lab1`);
+  res.status(200).send(`This is Lab1`);
 };
