c66f6fdd841086d3e18de23d544061df0ae0205c
rluCvVWc <panv1t2l1k@gmail.com>
2022-11-22 22:38:18 +1100

    add helpers/utils, update server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,43 @@
 import defaultHandler from './defaultHandler.js';
 import router from './router.js';
 import * as http from 'node:http';
+import helpers from './helpers.js';
+import safeJSON from './utils.js';
+
+const processedContentTypes = {
+  'text/html': (text) => text,
+  'text/plain': (text) => text,
+  'application/json': (json) => safeJSON(json, {}),
+  'x-www-form-urlencoded': (data) => {
+    return Object.fromEntries(new URLSearchParams(data));
+  },
+};
 
 const server = http.createServer(async (req, res) => {
   const url = new URL(req.url || '/', `https://${req.headers.host}`);
   const routerModule = router.get(url.pathname) ?? {};
   const handler = routerModule[req?.method] ?? defaultHandler;
 
-  handler(req, res, url);
+  let payload = {};
+  let rawRequest = '';
+
+  for await (const chunk of req) {
+    rawRequest += chunk;
+  }
+
+  if (req.headers['content-type']) {
+    const contentType = req.headers['content-type'].split(';')[0];
+    if (processedContentTypes[contentType]) {
+      payload = processedContentTypes[contentType](rawRequest);
+    }
+  }
+
+  try {
+    handler(req, Object.assign(res, helpers), url, payload, rawRequest);
+  } catch (e) {
+    res.statusCode = 500;
+    res.end(process.env.NODE_ENV === 'production' ? 'internal error' : e);
+  }
 });
 
 server.on('clientError', (err, socket) => {
