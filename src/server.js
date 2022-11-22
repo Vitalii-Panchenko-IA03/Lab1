48d3609d21ce2471a94a2730b3b595b5c6414e98
rluCvVWc <panv1t2l1k@gmail.com>
2022-11-22 22:03:19 +1100

    add defaultHandler, update server

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import defaultHandler from './defaultHandler.js';
+import router from './router.js';
+import * as http from 'node:http';
+
+const server = http.createServer(async (req, res) => {
+  const url = new URL(req.url || '/', `https://${req.headers.host}`);
+  const routerModule = router.get(url.pathname) ?? {};
+  const handler = routerModule[req?.method] ?? defaultHandler;
+
+  handler(req, res, url);
+});
+
+server.on('clientError', (err, socket) => {
+  if (err) {
+    console.log(err.stack);
+  }
+  socket.end('HTTP/1.1 400 bad request\r\n\r\n');
+});
+server.listen(parseInt(process.env.PORT) || 8000);
+
+process.on('SIGINT', () => {
+  server.close((error) => {
+    if (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  });
+});
